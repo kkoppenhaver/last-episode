70ab7d747fd872fe06017e52ccc44d6437c5d9ce
Keanan Koppenhaver <keanan@howtogeek.com>
2017-08-05 11:52:38 -0400

    Updates as per Amazon certification team

--- skill.py
+++ skill.py
@@ -12,17 +12,21 @@ def lambda_handler(event, context):
         
     ## Handle the bare launch intent
     if(event["request"]["type"] == "LaunchRequest"):
-        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, when was the last episode of How I Met Your Mother?", False)
+        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, you can say<break time='100ms' strength='weak'/>When was the last episode of How I Met Your Mother?", False)
         
     intent_name = event["request"]["intent"]["name"]
     
     ## Handle the help intent
     if intent_name == "AMAZON.HelpIntent":
-        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, when was the last episode of How I Met Your Mother?", False)
+        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, you can say<break time='100ms' strength='weak'/>When was the last episode of How I Met Your Mother?", False)
     
     ## Handle the stop intent
     elif intent_name == "AMAZON.StopIntent" or intent_name == "AMAZON.CancelIntent":
         return build_response( "Thanks!", "Thanks for using Last Episode. Goodbye!", True)
+        
+    ## Handle the "help" command
+    elif intent_name == "TroubleshootIntent":
+      return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, you can say<break time='100ms' strength='weak'/>When was the last episode of How I Met Your Mother?", False)
     
     elif intent_name == "GetLastEpisodeIntent":
         return query_series(event["request"]["intent"]["slots"]["series_name"]["value"])
@@ -33,7 +37,7 @@ def query_series(series_name):
     should_end_session = True
     
     api_url = API_BASE + "singlesearch/shows?" + urllib.urlencode({"q": series_name})
-    
+
     response = urllib2.urlopen(api_url)
   
     series_data = json.load(response)
@@ -43,15 +47,16 @@ def query_series(series_name):
     prev_ep_response = urllib2.urlopen(prev_ep_link)
 
     prev_ep_data = json.load(prev_ep_response)
-
-    prev_ep_data["summary"] = re.sub('<[^<]+?>', '', prev_ep_data["summary"])
+    
+    if(prev_ep_data["summary"]):
+        prev_ep_data["summary"] = re.sub('<[^<]+?>', '', prev_ep_data["summary"])
 
     prev_ep_date = datetime.datetime.strptime(prev_ep_data["airstamp"], "%Y-%m-%dT%H:%M:%S+00:00").date()
     prev_ep_date = prev_ep_date.strftime('%A %B %-d, %Y')
     
     prev_ep_summary = prev_ep_data["summary"]
     
-    if(prev_ep_summary != ''):
+    if(prev_ep_summary and prev_ep_summary != ''):
         prev_ep_summary = "In this episode, " + prev_ep_summary
     else:
         prev_ep_summary = "No summary was found."
@@ -82,6 +87,3 @@ def build_response( title, output_text, should_end_session ):
     },
     "sessionAttributes": {}
   }
-    
-    
-    
