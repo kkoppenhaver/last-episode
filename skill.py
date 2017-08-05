7e2e845c8d18283712a4d805f5f32477adeac59c
Keanan Koppenhaver <keanan@howtogeek.com>
2017-08-05 11:17:33 -0400

    Handling empty episode summary case

--- skill.py
+++ skill.py
@@ -12,13 +12,13 @@ def lambda_handler(event, context):
         
     ## Handle the bare launch intent
     if(event["request"]["type"] == "LaunchRequest"):
-        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, when was the last episode of How I Met Your Mother?", True)
+        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, when was the last episode of How I Met Your Mother?", False)
         
     intent_name = event["request"]["intent"]["name"]
     
     ## Handle the help intent
     if intent_name == "AMAZON.HelpIntent":
-        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, when was the last episode of How I Met Your Mother?", True)
+        return build_response("Last Episode", "Welcome to Last Episode. Ask me about a T.V. series. For example, when was the last episode of How I Met Your Mother?", False)
     
     ## Handle the stop intent
     elif intent_name == "AMAZON.StopIntent" or intent_name == "AMAZON.CancelIntent":
@@ -30,12 +30,12 @@ def lambda_handler(event, context):
 def query_series(series_name):
     title = "Last Episode of " + series_name
     output_text = "You asked for the last episode of " + series_name
-    should_end_session = False
+    should_end_session = True
     
     api_url = API_BASE + "singlesearch/shows?" + urllib.urlencode({"q": series_name})
     
     response = urllib2.urlopen(api_url)
-    
+  
     series_data = json.load(response)
 
     prev_ep_link = series_data["_links"]["previousepisode"]["href"]
@@ -48,30 +48,40 @@ def query_series(series_name):
 
     prev_ep_date = datetime.datetime.strptime(prev_ep_data["airstamp"], "%Y-%m-%dT%H:%M:%S+00:00").date()
     prev_ep_date = prev_ep_date.strftime('%A %B %-d, %Y')
+    
+    prev_ep_summary = prev_ep_data["summary"]
+    
+    if(prev_ep_summary != ''):
+        prev_ep_summary = "In this episode, " + prev_ep_summary
+    else:
+        prev_ep_summary = "No summary was found."
 
-    output_text = prev_ep_data["name"] + ", episode " + str(prev_ep_data["number"]) + " of season " + str(prev_ep_data["season"]) + ", aired on " + prev_ep_date + ". In this episode, " + prev_ep_data["summary"]
+    output_text = prev_ep_data["name"] + ", episode " + str(prev_ep_data["number"]) + " of season " + str(prev_ep_data["season"]) + ", aired on " + prev_ep_date + ". " + prev_ep_summary
     return build_response(title, output_text, should_end_session)
     
 def build_response( title, output_text, should_end_session ):
     return {
-      "version": "1.0",
-      "response": {
+    "version": "1.0",
+    "response": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": output_text
+      },
+      "card": {
+        "content": output_text,
+        "title": title,
+        "type": "Simple"
+      },
+      "reprompt": {
         "outputSpeech": {
           "type": "PlainText",
-          "text": output_text
-        },
-        "card": {
-          "content": output_text,
-          "title": title,
-          "type": "Simple"
-        },
-        "reprompt": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": ""
-          }
-        },
-        "shouldEndSession": should_end_session
+          "text": ""
+        }
       },
-      "sessionAttributes": {}
-    }
+      "shouldEndSession": should_end_session
+    },
+    "sessionAttributes": {}
+  }
+    
+    
+    
